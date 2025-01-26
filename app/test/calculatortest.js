92288c0253abf6c0fdf227dc4571f8154e87b857
destowq@gmail.com <destowq@gmail.com>
2025-01-26 01:32:10 -0500

    lab2 -edited

--- app/test/calculatortest.js
+++ app/test/calculatortest.js
@@ -4,35 +4,35 @@ const Arithmetic = require('../calculator')
 
 describe("Validating Arithmetic functions", () =>{
     
-    it("add(5, 2) expected result 7", ()=>{
+    it("add(5, 2) expected result 7 PASS", ()=>{
         assert.equal(Arithmetic.add(5,2),7)
     })
 
-    it("add(5,2) expected result 8", ()=>{
-        assert.equal(Arithmetic.add(5,2),8)
+    it("add(5,2) expected result 8 FAIL", ()=>{
+        assert.notEqual(Arithmetic.add(5,2),8)
     })
     
-    it("sub(5, 2) expected result 3", ()=>{
+    it("sub(5, 2) expected result 3 PASS", ()=>{
         assert.equal(Arithmetic.sub(5,2),3)
     })
 
-    it("sub(5,2) expected result 5", ()=>{
-        assert.equal(Arithmetic.sub(5,2),5)
+    it("sub(5,2) expected result 5 FAIL", ()=>{
+        assert.notEqual(Arithmetic.sub(5,2),5)
     })
 
-    it("mul(5, 2) expected result 10", ()=>{
+    it("mul(5, 2) expected result 10 PASS", ()=>{
         assert.equal(Arithmetic.mul(5, 2), 10)
     })
 
-    it("mul(5,2) expected result 12", ()=>{
-        assert.equal(Arithmetic.mul(5, 2), 12)
+    it("mul(5,2) expected result 12 FAIL", ()=>{
+        assert.notEqual(Arithmetic.mul(5, 2), 12)
     })
 
-    it("div(10, 2) expected result 5", ()=>{
+    it("div(10, 2) expected result 5 PASS", ()=>{
         assert.equal(Arithmetic.div(10, 2), 5)
     })
-    it("div(10,2) expected result 2", ()=>{
-        assert.equal(Arithmetic.div(10, 2), 2)
+    it("div(10,2) expected result 2 FAIL", ()=>{
+        assert.notEqual(Arithmetic.div(10, 2), 2)
     })
 
 } )
